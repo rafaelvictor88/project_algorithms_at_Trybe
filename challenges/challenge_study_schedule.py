4c7a92098cb7890c7c26591d2cf95fc46af708ab
Rafael Victor <rafavictorgs@gmail.com>
2022-04-20 15:26:31 -0300

    Req: Resolve requisito 1 - arquivo challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    count_target_time = 0
+    for students in permanence_period:
+        if type(students[0]) != int or type(students[1]) != int:
+            return None
+        # Solução lembrada pelo Gabriel Suassuna (entre valores);
+        if students[0] <= target_time <= students[1]:
+            count_target_time += 1
+    return count_target_time
